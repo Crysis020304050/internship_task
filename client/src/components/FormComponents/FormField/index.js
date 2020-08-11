645f9453c9b3a3302b0cde0b3368c074457e9189
Danyil Zatserkovnyi <dzatserkovnyi@freshcode.org>
2020-08-11 14:31:42 +0300

    Replaced my own field components with material-ui TextField

--- client/src/components/FormComponents/FormField/index.js
+++ client/src/components/FormComponents/FormField/index.js
@@ -1,32 +1,18 @@
 import React from "react";
-import FieldError from "../FieldError";
-import FieldInput from "../FieldInput";
-import FieldInputWrapper from "../FieldInputWrapper";
-import PropTypes from "prop-types";
-import classNames from 'classnames';
 import styles from './FormField.module.sass';
+import TextField from '@material-ui/core/TextField';
 
-const FormField = ({label, type, inputStyle, input, meta: {touched, error, visited}, warningStyle, validStyle, invalidStyle, containerStyle}) => {
-
-    const inputClassName = classNames(styles.inputStyle, inputStyle, {
-        [invalidStyle || styles.invalidStyle]: touched && error,
-        [validStyle || styles.validStyle]: visited && !error,
-    });
-
-    return (
-        <FieldInputWrapper className={classNames(styles.containerStyle, containerStyle)}>
-            <FieldInput input={input} className={inputClassName} label={label} type={type}/>
-            <FieldError className={warningStyle} error={error} touched={touched}/>
-        </FieldInputWrapper>
-    );
-};
-
-FormField.propTypes = {
-    validStyle: PropTypes.string,
-    invalidStyle: PropTypes.string,
-    inputStyle: PropTypes.string,
-    containerStyle: PropTypes.string,
-    warningStyle: PropTypes.string
-};
-
+const FormField = ({label, type, input, meta: {error}}) => (
+    <div className={styles.containerStyle}>
+        <TextField
+            className={styles.inputStyle}
+            error={error}
+            {...input}
+            label={label}
+            type={type}
+            variant="filled"
+            helperText={error}
+        />
+    </div>
+);
 export default FormField;
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import {Field} from 'redux-form';
 import FormField from "../components/FormComponents/FormField";
 
-export const renderFields = (fieldsData, customClasses = []) => (
-    fieldsData.map((fieldData, index) => (
-        <Field key={index} {...fieldData} {...customClasses} component={FormField}/>
+export const renderFields = (fieldsData = []) => (
+    fieldsData.map(fieldData => (
+        <Field key={fieldData.id} {...fieldData} component={FormField}/>
     ))
 );
