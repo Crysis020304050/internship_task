b986b2187b31291ac90b70bbb45d065ee23b9498
Danyil Zatserkovnyi <dzatserkovnyi@freshcode.org>
2020-08-14 12:26:18 +0300

    Create axios interceptor on response errors
    Add refreshTokens query to this interceptor
    Refactor some code

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,13 +1,46 @@
 import axios from 'axios';
 import constants from "../constants";
+import history from "../browserHistory";
+import {refreshTokens} from "./authentication";
+import {setTokens, clearStorage} from "../utils";
+
+const {LINKS: {BASE_URL}, TOKENS: {ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY}} = constants;
 
 const instance = axios.create({
-    baseURL: constants.LINKS.BASE_URL,
+    baseURL: BASE_URL,
 });
 
 instance.interceptors.request.use(config => {
-    config.headers.authorization = sessionStorage.getItem(constants.TOKENS.ACCESS_TOKEN_KEY);
+    config.headers.authorization = sessionStorage.getItem(ACCESS_TOKEN_KEY);
     return config;
 }, (err) => Promise.reject(err));
+
+instance.interceptors.response.use(response => response, async err => {
+    const {response: {status}, config} = err;
+    switch (status) {
+        case 419: {
+            const {data} = await refreshTokens({
+                refreshToken: localStorage.getItem(REFRESH_TOKEN_KEY),
+            });
+            if (data) {
+                setTokens(data);
+                return instance.request(config);
+            } else {
+                clearStorage();
+                history.replace('/login');
+            }
+            break;
+        }
+        case 401: {
+            clearStorage();
+            history.replace('/login');
+            break;
+        }
+        default: {
+            return Promise.reject(err);
+        }
+    }
+    return Promise.reject(err);
+});
 
 export default instance;
