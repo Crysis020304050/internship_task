cc4f3fd08a588f4dcb8af54a7997c4b25f8f61ab
Danyil Zatserkovnyi <dzatserkovnyi@freshcode.org>
2020-08-14 18:13:17 +0300

    Add basic use cards view into home page

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, {useEffect} from 'react';
 import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
 import styles from './HomePage.module.sass';
-import Header from "../../components/Header";
+import Header from '../../components/Header';
+import {getUsersRequest} from '../../actions';
+import {connect} from 'react-redux';
+import InfinityScrollListContainer from "../../components/InfinityScrollListContainer";
+import TryAgain from "../../components/TryAgain";
+import UserInfoCard from "../../components/UserInfoCard";
 
-const HomePage = props => {
+const HomePage = ({getUsers, users, error, isFetching, hasMore}) => {
+
+    useEffect(() => {
+        getUsers({limit: 8});
+    }, []);
+
+    const loadMore = (offset) => {
+        getUsers({
+            limit: 8,
+            offset,
+        });
+    };
+
+    const renderUserCard = () => users.map(user => <UserInfoCard key={user.id} {...user}/>);
 
     return (
         <Container className={styles.mainContainer}>
             <Header/>
+            {
+                error
+                    ? <TryAgain getData={() => getUsers({limit: 8})}/>
+                    : <InfinityScrollListContainer isFetching={isFetching} hasMore={hasMore} loadMore={loadMore} className={styles.cardContainer}>
+                        {
+                            renderUserCard()
+                        }
+                    </InfinityScrollListContainer>
+            }
         </Container>
     );
 };
 
-export default HomePage;
+const mapStateToProps = state => state.usersDataStore;
+
+const mapDispatchToProps = dispatch => ({
+    getUsers: (filter) => dispatch(getUsersRequest(filter)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
 
